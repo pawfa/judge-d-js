69b97ff26072e8d33a3b3bb67da3270f7c5e1c5b
Pawfa <fajfer.pawel@gmail.com>
2021-03-09 14:14:25 +0100

    added old workflow with 4 spaces indentation

--- prettier.config.js
+++ prettier.config.js
@@ -2,12 +2,4 @@ module.exports = {
     singleQuote: true,
     tabWidth: 4,
     trailingComma: 'es5',
-    overrides: [
-        {
-            files: ['./**/*.yml'],
-            options: {
-                tabWidth: 2
-            }
-        }
-    ]
 };
